dea8bce69c21bb2bfbae9fd7575b98967c9aea94
Adam Field <adamjfield@gmail.com>
2021-12-05 14:27:45 -0600

    added addNote function

--- db/store.js
+++ db/store.js
@@ -23,6 +23,17 @@ class Store {
       return parsedNotes;
     });
   }
+  addNote(note) {
+    const { title, text } = note;
+    if (!title || !text) {
+      throw new Error("note 'title' and 'text' cannot be blank!");
+    }
+    const newNote = { title, text, id: uuidv4() };
+    return this.getNotes()
+      .then(notes => [...notes, newNote])
+      .then(updatedNotes => this.write(updatedNotes))
+      .then(() => newNote);
+  }
 }
 
 module.exports = new Store();

26790c6803c87804cbf520134ca6b02f21d75d64
Adam Field <adamjfield@gmail.com>
2021-12-05 14:27:07 -0600

    added getNotes function

--- db/store.js
+++ db/store.js
@@ -5,6 +5,24 @@ const { v4: uuidv4 } = require('uuid');
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-class Store {}
+class Store {
+  read() {
+    return readFileAsync('db/db.json', 'utf-8');
+  }
+  write(note) {
+    return writeFileAsync('db/db.json', JSON.stringify(note));
+  }
+  getNotes() {
+    return this.read().then(notes => {
+      let parsedNotes;
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        parsedNotes = [];
+      }
+      return parsedNotes;
+    });
+  }
+}
 
 module.exports = new Store();

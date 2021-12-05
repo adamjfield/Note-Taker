4c8b7e42cb3344f1379d86fe2794db28bf82aed9
Adam Field <adamjfield@gmail.com>
2021-12-05 14:28:35 -0600

    added removeNote function

--- db/store.js
+++ db/store.js
@@ -34,6 +34,11 @@ class Store {
       .then(updatedNotes => this.write(updatedNotes))
       .then(() => newNote);
   }
+  removeNote(id) {
+    return this.getNotes()
+      .then(notes => notes.filter(note => note.id !== id))
+      .then(filterNotes => this.write(filterNotes));
+  }
 }
 
 module.exports = new Store();

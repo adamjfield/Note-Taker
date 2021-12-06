84a27e18574d1be13ef0b7f091998be6096d482e
Adam Field <adamjfield@gmail.com>
2021-12-06 10:44:12 -0600

    fix errors

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,22 +4,22 @@ const store = require('../db/store');
 router.get('/notes', (req, res) => {
   store
     .getNotes()
-    .then((notes) => {
+    .then(notes => {
       return res.json(notes);
     })
-    .catch((err) => res.status(500).json(err));
+    .catch(err => res.status(500).json(err));
 });
 router.post('/notes', (req, res) => {
   store
     .addNote(req.body)
-    .then((note) => res.json(note))
-    .catch((err) => res.status(500).json(err));
+    .then(note => res.json(note))
+    .catch(err => res.status(500).json(err));
 });
 router.delete('/notes/:id', (req, res) => {
   store
     .removeNote(req.params.id)
     .then(() => res.json({ ok: true }))
-    .catch((err) => res.status(500).json(err));
+    .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
